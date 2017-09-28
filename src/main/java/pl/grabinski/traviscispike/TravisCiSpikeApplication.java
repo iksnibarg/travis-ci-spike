8a9a0c293e13c65dc7e0cf5603e6cb01c20aedd5
grabinsk <maciej.grabinski@nokia.com>
2017-09-28 10:26:58 +0200

    Creating basic Spring Boot application (generated using start.spring.io)

---
+++ src/main/java/pl/grabinski/traviscispike/TravisCiSpikeApplication.java
@@ -0,0 +1,12 @@
+package pl.grabinski.traviscispike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisCiSpikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisCiSpikeApplication.class, args);
+	}
+}
